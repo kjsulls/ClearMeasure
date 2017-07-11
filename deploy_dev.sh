507ab099d5a27f05758d91c7fa5039af7c5ef25f
ksullivan <ksullivan@ngagelive.com>
2017-07-10 20:37:48 -0500

    updating bash

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-ssh -v root@github.com:kjsulls/ClearMeasure.git
-
 echo '1. Updating sources'
-cd c:/Projects/ClearMeasure
+cd  c:/Projects/ClearMeasure
 git checkout --force master
 git pull
 
